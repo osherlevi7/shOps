23246efb27971378455eaf0775a4acd0d223df40
osherlevi7 <osher.levi7@gmail.com>
2022-05-23 18:15:14 +0300

    feat: add service for jenkins slave

---
+++ fileSystem/services/jenkins/slave.sh
@@ -0,0 +1,2 @@
+java -jar agent.jar -jnlpUrl https://jenkins.url.to.your.instance.com/computer/slave-name/jenkins-agent.jnlp -secret 8f2437fc9cf0a17efd24de0828bc8d590d3 -workDir "/path/to-the-slave/work-dir"
+exit 0
